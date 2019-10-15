10c35523f1ee9c4970fa8279f56ce746bee8db6e
Ihor Barmak <ihor@uplab.io>
2019-10-15 20:39:13 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new CopyPlugin([
+      { from: 'public', to: '.' },
+    ]),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [ {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }],
+      }
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js'
+  },
+};
