3fa1b9cf809b70990a44089b31eeb8ed980fa9c8
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2019-10-22 16:55:00 +0300

    add: save posts in localStorage

--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -3,13 +3,18 @@ import Feed from '../components/Feed';
 import PostForm from '../components/PostForm';
 // import posts from '../__mocks__/posts';
 
+let posts = localStorage.getItem('posts');
+posts = posts?JSON.parse(localStorage.getItem('posts')):[];
+
 class FeedPage extends Component {
   state = {
-    posts: [],
+    posts: posts,
   }
 
   handleAddPost = (post) => {
     this.setState((prevState) => ({ posts: [post, ...prevState.posts] }));
+
+    setTimeout(()=>{ localStorage.setItem('posts', JSON.stringify(this.state.posts)); }, 2000);
   }
 
   render() {
