bb8d5e6ac820ad6512f862a4bafc335f67e9e66f
Oksenyuk Dmytro <Oksenyuk.Dmitro@ukr.net>
2019-11-10 11:39:41 +0200

    feat: added update profile

--- src/hooks/useUserForm.js
+++ src/hooks/useUserForm.js
@@ -1,14 +1,41 @@
-import { useMemo, useState, useCallback } from 'react';
-import UsersModel from '../modules/users';
+import { useState, useCallback } from 'react';
+import { toast } from 'react-toastify';
+import _ from 'lodash';
+import { useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import Uploader from '../modules/uploader';
+import useMe from './useMe';
 
-const useUserForm = (user) => {
-  const currentUser = useMemo(() => user || UsersModel.me(), [user]);
+
+const UPDATE_ACCOUNT_MUTATION = gql`
+mutation updateAccount ($username: String! $profile: UserProfileInput!) {
+  updateAccount (
+    input:{
+      username: $username      
+      profile: $profile
+    }
+  ) {
+    _id
+        username   
+     profile {
+      firstName
+      lastName
+      avatar
+    }   
+  }
+}
+`;
+const useUserForm = () => {
+  const [updateAccount] = useMutation(UPDATE_ACCOUNT_MUTATION);
+
+  const [me] = useMe();
   const [state, setState] = useState({
-    avatar: currentUser.profile.avatar,
-    username: currentUser.username,
-    firstName: currentUser.profile.firstName,
-    lastName: currentUser.profile.lastName,
-    password: '',
+    avatar: me.profile.avatar,
+    newAvatar: null,
+    username: me.username,
+    firstName: me.profile.firstName,
+    lastName: me.profile.lastName,
+    isUploading: false,
   });
 
   const handleChange = useCallback((e) => {
@@ -16,7 +43,48 @@ const useUserForm = (user) => {
     setState((s) => ({ ...s, [name]: value }));
   }, []);
 
-  return [state, handleChange];
+  const validation = useCallback(() => {
+    const {
+      firstName, lastName, username,
+    } = state;
+
+    if (_.isEmpty(firstName) || _.isEmpty(lastName) || _.isEmpty(username)) throw new Error('invalid date');
+    return true;
+  }, [state]);
+
+  const uploadAvatar = async (newAvatar) => {
+    const url = await Uploader.upload(newAvatar);
+    return url;
+  };
+
+  const handleSubmit = useCallback(async (e) => {
+    e.preventDefault();
+    let newAvatar = e.target.newAvatar.files[0];
+    const {
+      username, firstName, lastName, avatar,
+    } = state;
+    const profile = { firstName, lastName, avatar };
+    try {
+      validation();
+      if (newAvatar) {
+        setState((s) => ({ ...s, isUploading: true }));
+        newAvatar = await uploadAvatar(newAvatar) || avatar;
+        setState((s) => ({ ...s, avatar: newAvatar, isUploading: false }));
+        profile.avatar = newAvatar;
+      }
+
+      await updateAccount({
+        variables: { username, profile },
+      });
+      toast.success('Success');
+    } catch (error) {
+      setState((s) => ({ ...s, isUploading: false }));
+      toast.error(error.message);
+    }
+  }, [state, updateAccount, validation]);
+
+  return [state, handleChange, handleSubmit];
 };
+
 
 export default useUserForm;
--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,93 +1,18 @@
-import React, {
-  useContext, useState, useCallback,
-} from 'react';
+import React, { useContext } from 'react';
 import {
   Col, Form, Button, FormGroup, Label, Input, Media, Spinner,
 } from 'reactstrap';
-import { toast } from 'react-toastify';
-import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import ThemeContext from '../contexts/Theme';
-import UsersModel from '../modules/users';
-import Uploader from '../modules/uploader';
 import Select from '../components/Select';
-import useMe from '../hooks/useMe';
 import routes from '../router/routes';
-
-const useUserForm = () => {
-  const [me] = useMe();
-  const [state, setState] = useState({
-    avatar: me.profile.avatar,
-    newAvatar: null,
-    username: me.username,
-    firstName: me.profile.firstName,
-    lastName: me.profile.lastName,
-    password: '',
-    newPassword: '',
-    isUploading: false,
-  });
-
-  const handleChange = useCallback((e) => {
-    const { name, value } = e.target;
-    setState((s) => ({ ...s, [name]: value }));
-  }, []);
-
-  const validation = useCallback(() => {
-    const {
-      firstName, lastName, password, newPassword, username,
-    } = state;
-    const userByUserName = UsersModel.getUserByUsername(username);
-    const encryptPassword = UsersModel.encrypt(password);
-    if (!_.isEmpty(password) && encryptPassword !== me.password) throw new Error('invalid password');
-    if (!_.isEmpty(password) && _.isEmpty(newPassword)) throw new Error('invalid  new password');
-    if (userByUserName && userByUserName.id !== me.id) throw new Error('Username already taken!');
-    if (_.isEmpty(firstName) || _.isEmpty(lastName) || _.isEmpty(username)) throw new Error('invalid date');
-    return true;
-  }, [me.id, me.password, state]);
-
-  const uploadAvatar = async (newAvatar) => {
-    const url = await Uploader.upload(newAvatar);
-    return url;
-  };
-
-  const handleSubmit = useCallback(async (e) => {
-    e.preventDefault();
-    let newAvatar = e.target.newAvatar.files[0];
-    const {
-      firstName, lastName, password, newPassword, username, avatar,
-    } = state;
-    const newUser = {
-      id: me.id,
-      username,
-      password: _.isEmpty(password) ? me.password : UsersModel.encrypt(newPassword),
-      profile: { firstName, lastName, avatar },
-    };
-    try {
-      validation();
-      if (newAvatar) {
-        setState((s) => ({ ...s, isUploading: true }));
-        newAvatar = await uploadAvatar(newAvatar) || avatar;
-        setState((s) => ({ ...s, avatar: newAvatar, isUploading: false }));
-        newUser.profile.avatar = newAvatar;
-      }
-
-      UsersModel.update(newUser);
-      me.password = newUser.password;
-      toast.success('Success');
-    } catch (error) {
-      setState((s) => ({ ...s, isUploading: false }));
-      toast.error(error.message);
-    }
-  }, [me.id, me.password, state, validation]);
-
-  return [state, handleChange, handleSubmit];
-};
+import useUserForm from '../hooks/useUserForm';
 
 
 const UserProfilePage = () => {
   const [state, handleChange, handleSubmit] = useUserForm();
   const {
-    username, firstName, lastName, password, avatar, newPassword, isUploading,
+    username, firstName, lastName, avatar, isUploading,
   } = state;
 
   const { theme, setTheme } = useContext(ThemeContext);
@@ -140,35 +65,7 @@ const UserProfilePage = () => {
             <Input value={lastName} name="lastName" onChange={handleChange} />
           </Col>
         </FormGroup>
-        <FormGroup row>
-          <Label for="password" sm={3}>
-            password
-          </Label>
-          <Col sm={9}>
-            <Input
-              type="password"
-              placeholder="password"
-              name="password"
-              value={password}
-              onChange={handleChange}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="new password" sm={3}>
-            New password
-          </Label>
-          <Col sm={9}>
-            <Input
-              type="password"
-              placeholder="new password"
-              name="newPassword"
-              value={newPassword}
-              onChange={handleChange}
-              disabled={password.length === 0}
-            />
-          </Col>
-        </FormGroup>
+
         <FormGroup row>
           <Label sm={3}>
             Avatar
@@ -184,17 +81,13 @@ const UserProfilePage = () => {
           <Link to={routes.changePassword}>
             <Button className="button" type="button" style={{ float: 'left', marginLeft: '0' }}>  Change Password</Button>
           </Link>
-
-
           <Button
             style={{ float: 'right' }}
             className="button"
           >
             Save changes
           </Button>
-
         </Col>
-
       </Form>
 
 
